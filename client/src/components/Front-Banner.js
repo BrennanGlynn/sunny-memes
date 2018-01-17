b216773fc00aba79dddfc883a53b16dccecd36c9
Ben Jeske <ben@fortifiedhq.com>
2018-01-16 22:02:19 -0700

    Getting ready to edit banner

--- client/src/components/Front-Banner.js
+++ client/src/components/Front-Banner.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class Banner extends Component {
     render() {
-        return(<h1>Banner</h1>);
+        return(
+          <h1>Banner</h1>
+        );
     }
 }
 
 export default Banner;
-

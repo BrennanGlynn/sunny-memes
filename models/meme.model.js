8f81304e68df81029065c5e64d671b749eef77f8
Brennan Glynn <brennanglynn@u.boisestate.edu>
2018-01-28 05:38:57 -0700

    Set up app max characters

--- models/meme.model.js
+++ models/meme.model.js
@@ -3,32 +3,33 @@ const Schema = mongoose.Schema;
 
 // Set up new meme schema
 const MemeSchema = new Schema({
-    url: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    title: {
-        type: String,
-        require: true,
-    },
-    uploaded_by: {
-        type: String,
-        required: true
-    },
-    characters: {
-        type: [String],
-        required: true
-    },
-    tags: [String],
-    visits: {
-        type: Number,
-        default: 0
-    },
-    favorites: {
-        type: [String],
-        default: []
-    }
+  url: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  title: {
+    type: String,
+    require: true,
+    maxlength: 65
+  },
+  uploaded_by: {
+    type: String,
+    required: true
+  },
+  characters: {
+    type: [String],
+    required: true
+  },
+  tags: [String],
+  visits: {
+    type: Number,
+    default: 0
+  },
+  favorites: {
+    type: [String],
+    default: []
+  }
 })
 
 module.exports = mongoose.model('Meme', MemeSchema);

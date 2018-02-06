45a53774eef372b10cfd0e760e7785d45c8338a6
Brennan Glynn <brennanglynn@u.boisestate.edu>
2018-02-06 16:19:49 -0700

    removed unnecessary code

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const User = require('../models/user.model');
-
-passport.serializeUser(function (user, done) {
-    done(null, user._id);
-});
-
-passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
-        done(err, user)
-    })
-});
 
 router.get('/facebook', passport.authenticate('facebook'), { authType: 'request', scope: ['public_profile', 'email']});
 

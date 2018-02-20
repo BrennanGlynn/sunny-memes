cbf42cf7f5f9b1f97b399ba4dd0ed2f77bc1e3c7
Brennan Glynn <brennanglynn@u.boisestate.edu>
2018-02-20 15:43:25 -0700

    removed traces of example data

--- client/src/components/pages/SingleMemePage.js
+++ client/src/components/pages/SingleMemePage.js
@@ -34,21 +34,6 @@ class SingleMemePage extends Component {
 
   render() {
     const { classes } = this.props;
-    const exampleData = {
-      _id: "5a837c342e0c281450425cf0",
-      title: "Raise a glass for the man in the room",
-      url: "/images/memes/1015659325615994758942.jpg",
-      uploaded_by: "10156593256159947",
-      favorites: [
-        "10156593256159947",
-        "10157285303858508"
-      ],
-      characters: [
-        "charlie",
-        "frank"
-      ],
-      numFaves: 2
-    }
 
     return(
       <div>
@@ -60,9 +45,6 @@ class SingleMemePage extends Component {
               (<MemeContainer meme={this.state.data} />) }
           </div>)
         }
-        Use this to design the single meme page.
-        Title: {exampleData.title}
-        Id: {this.props.match.params.id}
       </div>
     )
   }

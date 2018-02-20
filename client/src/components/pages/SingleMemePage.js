1ec2f5ff8efb2317bb7a8fc0c74fb58545ed1780
Ben Jeske <ben@fortifiedhq.com>
2018-02-20 14:36:45 -0700

    Added grid

--- client/src/components/pages/SingleMemePage.js
+++ client/src/components/pages/SingleMemePage.js
@@ -1,8 +1,17 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Grid from 'material-ui/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+});
 
 class SingleMemePage extends Component {
 
   render() {
+    const { classes } = this.props;
     const exampleData = {
       _id: "5a837c342e0c281450425cf0",
       title: "Raise a glass for the man in the room",
@@ -21,6 +30,14 @@ class SingleMemePage extends Component {
 
     return(
       <div>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={12} md={6}>
+            Meme card here
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+
+          </Grid>
+        </Grid>
         Use this to design the single meme page.
         Title: {exampleData.title}
 

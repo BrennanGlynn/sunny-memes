75e52aa6110345ad4fad29ce3196b90ed6091276
Ben Jeske <ben@fortifiedhq.com>
2018-02-20 16:07:40 -0700

    Removed unneeded styles

--- client/src/components/pages/SingleMemePage.js
+++ client/src/components/pages/SingleMemePage.js
@@ -1,7 +1,6 @@
 
 import React, {Component} from 'react'
 import PropTypes from 'prop-types';
-import {withStyles} from 'material-ui/styles';
 import { Grid, Paper } from 'material-ui/';
 import MemeContainer from "../../containers/memes/MemeContainer";
 
@@ -63,4 +62,4 @@ class SingleMemePage extends Component {
   }
 }
 
-export default withStyles(styles) (SingleMemePage);
+export default SingleMemePage

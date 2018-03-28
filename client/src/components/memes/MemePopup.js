2aa1859d8277027fe0bce2b7025e3242f748c351
Ben Jeske <ben@fortifiedhq.com>
2018-03-28 10:03:45 -0600

    Removed modal border

--- client/src/components/memes/MemePopup.js
+++ client/src/components/memes/MemePopup.js
@@ -102,7 +102,6 @@ const styles = theme => ({
       top: '50%',
       left: '50%',
       transform: 'translate(-50%, -50%)',
-      border: '1px solid #e5e5e5',
       backgroundColor: '#fff',
       boxShadow: '0 5px 15px rgba(0,0,0,.5)',
       padding: '32',
